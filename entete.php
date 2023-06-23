53f2280f5210a6d68859168c32279983a96de290
denene imbatia chancella <chancellaimbatiadenene@gmail.com>
2023-06-23 16:53:34 +0000

    ajout de mes dossiers de maquette, html et css

---
+++ entete.php
@@ -0,0 +1,60 @@
+<!--  <p>Ceci_concerne_entierement_l'entete</p> -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="stylentete.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="Entete">
+    <img class="Logo" src="image/Qalam_logo.png" alt="">
+    <img class="Covid" src="image/Covid.png" alt="">
+    <div class="Pub">
+        <img class="icone_pub" src="image/pub.png" alt="">
+    </div>
+    </div>
+    
+    <!--  <p>Ceci_concerne_entierement_la_barre_de_navigation</p> -->
+    <nav class="dropdownmenu">
+    <ul>
+        <li><a href="">QALAM INFO</a></li>
+        <li><a href="#">ACTUALITES</a>
+        <ul id="submenu">
+            <li><a href="">Actualités régionale</a></li>
+            <li><a href="">Actualités d'ailleurs</a></li>
+        </ul>
+        <li><a href="">EQUIPE</a></li>
+        <li><a href="">CONTACTEZ-NOUS</a></li>
+        <li><img class="icone_loupe" src="image/loupe.png" alt=""></li>
+        <li><a href=""><img class="icone_de_connection" src="image/compte.png" alt=""></a></li>
+        <li><img class="icone_doigt_de_direction" src="image/doigt_de_direction.png" alt=""></li>
+        <h4 class="connectez_vous">CONNECTEZ-VOUS <br> ICI</h4>
+        <h3 class="changer_de_langue">FR / EN</h3>
+    </ul>
+    </nav>
+    <!--  <p>Ceci concerne les pointiers verticalement superposé</p> -->
+    <div class="container_1">
+        <div class="box_noir"></div>
+        <div class="box_noir"></div>
+        <div class="box_noir"></div>
+        <div class="box_noir"></div>
+        <div class="box_noir"></div>
+        <div class="box_noir"></div>
+        <div class="box_noir"></div>
+    </div>
+    <div class="container_2">
+        <div class="box_noir"></div>
+        <div class="box_noir"></div>
+        <div class="box_noir"></div>
+        <div class="box_noir"></div>
+        <div class="box_noir"></div>
+        <div class="box_noir"></div>
+        <div class="box_noir"></div>
+    </div>
+</body>
+</html>
+
